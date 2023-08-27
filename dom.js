58fd0bfc9551aea7d3be17f9d16d21ef975a53bc
Jimcl6 <jedllorente@gmail.com>
2023-08-27 11:03:39 +0800

    learning about contro structures if/else/else if statements

--- dom.js
+++ dom.js
@@ -3,13 +3,28 @@ let sectionElement = document.getElementById("counter");
 let remainingCharElement = document.getElementById("remaining-chars");
 
 let inputElement = document.querySelector("input");
-let counter = 60;
 
 function checkUserInput() {
   let enteredText = inputElement.value;
+  let maxCharsInput = inputElement.maxLength;
 
-  let charactersEntered = counter - enteredText.length;
-  remainingCharElement.textContent = `${charactersEntered}/${counter}`;
+  let remainingChars = maxCharsInput - enteredText.length;
+
+  if (remainingChars === 0) {
+    inputElement.classList.add("error");
+    remainingCharElement.classList.add("error");
+  } else if (remainingChars <= 10) {
+    inputElement.classList.add("warning");
+    remainingCharElement.classList.add("warning");
+  } else {
+    inputElement.classList.remove("error", "warning");
+    remainingCharElement.classList.remove("error", "warning");
+  }
+
+  remainingCharElement.textContent = `${remainingChars}/${maxCharsInput}`;
 }
+let buttonElement = document.querySelector(".btn-reset");
+
+// buttonElement.removeEventListener();
 
 inputElement.addEventListener("input", checkUserInput);
