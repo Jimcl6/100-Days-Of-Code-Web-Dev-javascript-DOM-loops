fc91f7093628b7326a43a7a19c9af9aa1f008b66
jedllorente <jedllorente@gmail.com>
2023-08-31 16:28:58 +0800

    learning proper use of 'for...of' loop

---
+++ loops.js
@@ -0,0 +1,6 @@
+const users = ["Jed", "Ian", "Michael"];
+
+/* The code is using a `for...of` loop to iterate over each element in the `users` array. */
+for (const user of users) {
+  console.log(user);
+}
